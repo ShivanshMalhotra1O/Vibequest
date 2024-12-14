a13a1893216debfa5a3d7db2625e3a8a109676af
Mukund Mittal <mukund.mukulmittal@gmail.com>
2024-12-14 18:45:19 +0530

    Navbar Changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,46 @@
-export default function Navbar() {
-	return (
-		<nav className="sticky top-0 z-50 flex items-center justify-between w-full px-6 py-8 text-white bg-black lg:px-8 xl:px-16 backdrop-blur-sm">
-			<a href="/" className="flex items-center gap-4 group">
-				<img
-					src="/images/logo.png"
-					alt="Logo"
-					className="w-auto h-10 rounded-md"
-				/>
-				<h1 className="text-3xl font-bold">VibeQuest</h1>
-			</a>
+import {
+	Link,
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenu,
+	NavbarMenuItem,
+	NavbarMenuToggle,
+} from '@nextui-org/react';
 
-			<ul className="flex items-center gap-4 text-lg font-medium">
-				<li>
+export default function Nav() {
+	return (
+		<Navbar className="px-6 py-8 text-white bg-black lg:px-8 xl:px-16">
+			<NavbarContent>
+				<NavbarBrand>
+					<a href="/" className="flex items-center gap-4 group">
+						<img
+							src="/images/logo.png"
+							alt="Logo"
+							className="w-auto h-10 rounded-md"
+						/>
+						<h1 className="text-3xl font-bold">VibeQuest</h1>
+					</a>
+				</NavbarBrand>
+			</NavbarContent>
+
+			<NavbarContent justify="end" className="sm:hidden">
+				<NavbarMenuToggle />
+			</NavbarContent>
+			<NavbarContent
+				className="items-center hidden gap-4 text-lg font-medium sm:flex"
+				justify="end"
+			>
+				<NavbarItem>
 					<a
 						href="/"
 						className={window.location.pathname === '/' ? 'text-primary' : ''}
 					>
 						Home
 					</a>
-				</li>
-				<li>
+				</NavbarItem>
+				<NavbarItem>
 					<a
 						href="/about"
 						className={
@@ -28,8 +49,8 @@ export default function Navbar() {
 					>
 						About
 					</a>
-				</li>
-				<li>
+				</NavbarItem>
+				<NavbarItem>
 					<a
 						href="/login"
 						className={
@@ -38,8 +59,8 @@ export default function Navbar() {
 					>
 						Login
 					</a>
-				</li>
-				<li>
+				</NavbarItem>
+				<NavbarItem>
 					<a
 						href="/signup"
 						className={
@@ -48,8 +69,48 @@ export default function Navbar() {
 					>
 						Signup
 					</a>
-				</li>
-			</ul>
-		</nav>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarMenu className="bg-black mt-14">
+				<NavbarMenuItem>
+					<Link
+						href="/"
+						className={window.location.pathname === '/' ? 'text-primary' : ''}
+					>
+						Home
+					</Link>
+				</NavbarMenuItem>
+				<NavbarMenuItem>
+					<Link
+						href="/about"
+						className={
+							window.location.pathname === '/about' ? 'text-primary' : ''
+						}
+					>
+						About
+					</Link>
+				</NavbarMenuItem>
+				<NavbarMenuItem>
+					<Link
+						href="/login"
+						className={
+							window.location.pathname === '/login' ? 'text-primary' : ''
+						}
+					>
+						Login
+					</Link>
+				</NavbarMenuItem>
+				<NavbarMenuItem>
+					<Link
+						href="/signup"
+						className={
+							window.location.pathname === '/signup' ? 'text-primary' : ''
+						}
+					>
+						Signup
+					</Link>
+				</NavbarMenuItem>
+			</NavbarMenu>
+		</Navbar>
 	);
 }
